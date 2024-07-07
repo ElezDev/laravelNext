0525bb012555cdd2b172815eb60b78cf703cd99a
ElezDev <edwinledezma2023@gmail.com>
2024-07-07 15:29:18 -0500

    fix: componets

--- app/(app)/Navigation.tsx
+++ app/(app)/Navigation.tsx
@@ -1,90 +1,85 @@
-'use client'
-import { useState } from "react";
+"use client";
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { FaChartBar, FaUser, FaStethoscope } from "react-icons/fa";
-import Dashboard from "./dashboard/page";
-// Importa los componentes correspondientes
-import Tienda from "./tienda/page";
-import AssignRoles from "./AssignRoles/page";
+import { usePathname } from "next/navigation";
 
 const Navigation = ({ user }) => {
   const [open, setOpen] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
+  
+  const pathname = usePathname();
 
-  const Menus = [
+  const Links = [
     {
-      title: "Dashboard",
-      icon: <FaChartBar />,
-      component: <Dashboard />,
+      name: "Home",
+      path: "/dashboard",
       gap: true,
-    },
-    {
-      title: "Roles",
-      icon: <FaUser />,
-      component: <AssignRoles />,
-    },
-    {
-      title: "Tienda",
       icon: <FaStethoscope />,
-      component: <Tienda />,
+    },
+    {
+      name: "Tienda",
+      path: "/tienda",
       gap: true,
+      icon: <FaStethoscope />,
+    },
+    {
+      name: "Roles",
+      path: "/AssignRoles",
+      gap: true,
+      icon: <FaStethoscope />,
     },
   ];
 
-  const handleMenuClick = (index) => {
-    setActiveIndex(index);
-  };
+  useEffect(() => {
+    const index = Links.findIndex(link => link.path === pathname);
+    if (index !== -1) {
+      setActiveIndex(index);
+    }
+  }, [pathname]);
 
   return (
-    <div className="flex">
-      <div
-        className={`${
-          open ? "w-72" : "w-20"
-        } duration-300 h-screen p-5 pt-8 bg-blend-darken bg-green-600 relative`}
-      >
+    <div
+      className={`${
+        open ? "w-72" : "w-20"
+      } duration-300 h-screen p-5 pt-8 bg-blend-darken bg-green-600 relative`}
+    >
+      <img
+        src="/assets/arrow.png"
+        className={`absolute cursor-pointer rounded-full right-3 top-9 w-7 border-2 border-dark-purple ${
+          !open && "rotate-180"
+        }`}
+        onClick={() => setOpen(!open)}
+      />
+      <div className="flex gap-x-4 items-center">
         <img
-          src="/assets/arrow.png"
-          className={`absolute cursor-pointer rounded-full right-3 top-9 w-7 border-2 border-dark-purple ${
-            !open && "rotate-180"
-          }`}
-          onClick={() => setOpen(!open)}
+          src="/assets/elezdev.png"
+          className={`cursor-pointer duration-500`}
         />
-        <div className="flex gap-x-4 items-center">
-          <img
-            src="/assets/elezdev.png"
-            className={`cursor-pointer duration-500`}
-          />
-          <h1
-            className={`text-white origin-left font-medium text-xl duration-300 ${
-              !open && "scale-0"
-            }`}
-          >
-            Designer
-          </h1>
-        </div>
-        <ul className="pt-6">
-          {Menus.map((menu, index) => (
-            <li
-              className={`text-gray-300 text-sm flex items-center 
-                gap-x-4 cursor-pointer p-2 hover:bg-slate-400 rounded-md ${menu.gap ? "mt-9" : "mt-2"}`}
+        <h1
+          className={`text-white origin-left font-medium text-xl duration-300 ${
+            !open && "scale-0"
+          }`}
+        >
+          Designer
+        </h1>
+      </div>
+      <ul className="pt-6">
+        {Links.map((menu, index) => (
+          <li key={index} className={`text-white ${menu.gap ? "mt-9" : "mt-2"} ${index === activeIndex && "bg-gray-700 rounded-md"}`}>
+            <Link
               key={index}
-              onClick={() => handleMenuClick(index)}
+              href={menu.path}
+              className={`${
+                menu.path === pathname && "text-accent border-b-2 border-accent"
+              } capitalize font-medium hover:text-accent transition-all`}
             >
-              {menu.icon}
-              <span className={`${!open && "hidden"} origin-left duration-200`}>{menu.title}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-        {Menus.map((menu, index) =>
-          index === activeIndex && (
-            <div key={index}>
-              {menu.component} {/* Renderiza directamente el componente */}
-            </div>
-          )
-        )}
-      </div>
+              {menu.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,28 +1,29 @@
-'use client'
+"use client";
 
-import { ReactNode } from 'react'
-import { useAuth } from '../hooks/auth'
-import Navigation from './Navigation'
-import Loading from '@/app/(app)/Loading'
+import { ReactNode } from "react";
+import { useAuth } from "../hooks/auth";
+import Loading from "@/app/(app)/Loading";
+import Navigation from "./Navigation";
 
 interface AppLayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const AppLayout = ({ children }: AppLayoutProps) => {
-    const { user } = useAuth({ middleware: 'auth', redirectIfAuthenticated: false })
+  const { user } = useAuth({ middleware: "auth", redirectIfAuthenticated: false });
 
-    if (!user) {
-        return <Loading />
-    }
+  if (!user) {
+    return <Loading />;
+  }
 
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <Navigation user={user} />
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Navigation user={user} />
+      <main className="flex-1 p-7 text-2xl font-semibold h-screen">
+        {children}
+      </main>
+    </div>
+  );
+};
 
-            <main>{children}</main>
-        </div>
-    )
-}
-
-export default AppLayout
+export default AppLayout;
