a1fb2544b9d9ec2f7b750cd3bc153e2b74d68cd9
ElezDev <edwinledezma2023@gmail.com>
2024-07-07 12:08:32 -0500

    add: firs option

--- app/(app)/Navigation.tsx
+++ app/(app)/Navigation.tsx
@@ -1,172 +1,100 @@
-import ApplicationLogo from '../components/ApplicationLogo'
-import Dropdown from '../components/Dropdown'
-import Link from 'next/link'
-import NavLink from '../components/NavLink'
-import ResponsiveNavLink, {ResponsiveNavButton,} from '../components/ResponsiveNavLink'
-import { DropdownButton } from '../components/DropdownLink'
-import { useAuth } from '../hooks/auth'
-import { usePathname } from 'next/navigation'
-import { useState } from 'react'
-import { ReactNode } from 'react'
-
-
+import Link from 'next/link';
+import { useState } from 'react';
 
 const Navigation = ({ user }) => {
-    const { logout } = useAuth()
+  const [isOpen, setIsOpen] = useState(true);
 
-    const [open, setOpen] = useState(false)
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <nav className="bg-white border-b border-gray-100">
-            {/* Primary Navigation Menu */}
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16">
-                    <div className="flex">
-                        {/* Logo */}
-                        <div className="flex-shrink-0 flex items-center">
-                            <Link href="/dashboard">
-                                <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
-                            </Link>
-                        </div>
+  return (
+    <div className="flex">
+      {/* Sidebar */}
+      <div
+        className={`bg-gray-800 text-white fixed h-screen transition-all duration-300 z-10 ${
+          isOpen ? 'w-64' : 'w-0'
+        }`}
+      >
+        {/* Sidebar content */}
+        <div className="flex flex-col items-center">
+          <div className="mt-4">
+            <a
+              href="/dashboard"
+              className="text-white hover:text-gray-300"
+            >
+              Home
+            </a>
+          </div>
+          <div className="mt-4">
+            <a
+              href="/AssignRoles"
+              className="text-white hover:text-gray-300"
+            >
+              Roles
+            </a>
+          </div>
+          <div className="mt-4">
+            <a
+              href="/tienda"
+              className="text-white hover:text-gray-300"
+            >
+              Tienda
+            </a>
+          </div>
+          {/* Add more sidebar items here */}
+        </div>
+      </div>
+      {/* Main content */}
+      <div
+        className={`flex-1 p-4 transition-all duration-300 ${
+          isOpen ? 'ml-64' : 'ml-0'
+        }`}
+        style={{ marginLeft: isOpen ? '250px' : '0' }} // Ajuste adicional para evitar superposición
+      >
+        {/* Button to toggle sidebar */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={toggleSidebar}
+        >
+          {/* Toggle icon based on isOpen state */}
+          {isOpen ? (
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          )}
+        </button>
+        {/* Main content area */}
+        <div className="flex flex-col items-start">
+          {/* Your main content here */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                        {/* Navigation Links */}
-                        <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                            <NavLink
-                                href="/dashboard"
-                                active={usePathname() === '/dashboard'}>
-                                Dashboard
-                            </NavLink>
-
-                            <NavLink
-                                href="/tienda"
-                                active={usePathname() === '/tienda'}>
-                                Tienda
-                            </NavLink>
-
-                            <NavLink
-                                href="/AssignRoles"
-                                active={usePathname() === '/AssignRoles'}>
-                                Roles
-                            </NavLink>
-
-                           
-                        </div>
-                    </div>
-
-                    {/* Settings Dropdown */}
-                    <div className="hidden sm:flex sm:items-center sm:ml-6">
-                        <Dropdown
-                            align="right"
-                            width="48"
-                            trigger={
-                                <button className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
-                                    <div>{user?.name}</div>
-
-                                    <div className="ml-1">
-                                        <svg
-                                            className="fill-current h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20">
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clipRule="evenodd"
-                                            />
-                                        </svg>
-                                    </div>
-                                </button>
-                            }>
-                            {/* Authentication */}
-                            <DropdownButton onClick={logout}>
-                                Logout
-                            </DropdownButton>
-                        </Dropdown>
-                    </div>
-
-                    {/* Hamburger */}
-                    <div className="-mr-2 flex items-center sm:hidden">
-                        <button
-                            onClick={() => setOpen(open => !open)}
-                            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                            <svg
-                                className="h-6 w-6"
-                                stroke="currentColor"
-                                fill="none"
-                                viewBox="0 0 24 24">
-                                {open ? (
-                                    <path
-                                        className="inline-flex"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                ) : (
-                                    <path
-                                        className="inline-flex"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                )}
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            {/* Responsive Navigation Menu */}
-            {open && (
-                <div className="block sm:hidden">
-                    <div className="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink
-                            href="/dashboard"
-                            active={usePathname() === '/dashboard'}>
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-
-                    {/* Responsive Settings Options */}
-                    <div className="pt-4 pb-1 border-t border-gray-200">
-                        <div className="flex items-center px-4">
-                            <div className="flex-shrink-0">
-                                <svg
-                                    className="h-10 w-10 fill-current text-gray-400"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                                    />
-                                </svg>
-                            </div>
-
-                            <div className="ml-3">
-                                <div className="font-medium text-base text-gray-800">
-                                    {user?.name}
-                                </div>
-                                <div className="font-medium text-sm text-gray-500">
-                                    {user?.email}
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="mt-3 space-y-1">
-                            {/* Authentication */}
-                            <ResponsiveNavButton onClick={logout}>
-                                Logout
-                            </ResponsiveNavButton>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </nav>
-    )
-}
-
-export default Navigation
+export default Navigation;
